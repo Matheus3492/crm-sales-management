8c80d1d91918dfdb06d607ce8840bdc8ea5a5eef
Matheus Oliveira <groupmhr@hotmail.com>
2026-09-03 19:12:52 -0300

    feat: add user response schema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,14 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class UserCreate(BaseModel):
     name: str
     email: EmailStr
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+
+    model_config = ConfigDict(from_attributes=True)
