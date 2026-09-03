05249e57813bbb7e0acb827bf0dbfca22fef8783
Matheus Oliveira <groupmhr@hotmail.com>
2026-09-03 19:39:01 -0300

    refactor: organize schema exports

---
+++ app/schemas/__init__.py
@@ -0,0 +1,12 @@
+from app.schemas.customer import CustomerCreate, CustomerResponse
+from app.schemas.deal import DealCreate, DealResponse
+from app.schemas.user import UserCreate, UserResponse
+
+__all__ = [
+    "UserCreate",
+    "UserResponse",
+    "CustomerCreate",
+    "CustomerResponse",
+    "DealCreate",
+    "DealResponse",
+]
